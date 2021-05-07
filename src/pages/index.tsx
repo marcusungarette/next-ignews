3ad99aaf2d437155f34b3831a169616286fff237
marcusungarette <marcusungarette@gmail.com>
2021-05-07 16:42:40 -0300

    Added Global Style to application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,10 +6,7 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello
-        <span> World</span>
-      </h1>
+      <h1>Hello World</h1>
     </>
   );
 }
