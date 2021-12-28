9f10bdafdf6e73b2676f32cfec4bd89c67e2c2d1
marcusungarette <marcusungarette@gmail.com>
2021-12-28 15:00:50 -0300

    Added - SubscribeButton component with style and inserted on HomePage

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <>
+      <button 
+      type="button"
+      className={styles.subscribeButton}
+      >
+        Subscribe Now
+      </button>
+    </>
+  )
+}
