2cd15e9463059b494fb95350de927f6ca096e3f3
marcusungarette <marcusungarette@gmail.com>
2021-12-28 18:45:50 -0300

    Added Github OAuth using nextjs-OAuth config to SignIn button saving cookies, SignOut buton working too

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Marcus Ungarette
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+      >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
