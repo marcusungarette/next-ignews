ed4cf0020bfb9f96e9afe6980c93a3a630f19866
marcusungarette <marcusungarette@gmail.com>
2021-12-29 13:06:37 -0300

    Fix-Change FaunaDB Query - Added If/Else handle if userExists on DB (If User NotExists = Create One/ Else - Get and Login account when click button

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,13 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+
+interface signInProps {
+  email: {
+        verificationRequest?: boolean;
+    };
+}
 
 export default NextAuth({
   providers: [
@@ -13,4 +21,38 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            )
+          );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 })
