a4223ef674e664b33a36cbb4ba38430d24ac116b
marcusungarette <marcusungarette@gmail.com>
2021-12-29 14:27:59 -0300

    Feat - StripeJs SDK deal with Frontend req

---
+++ src/services/stripejs.ts
@@ -0,0 +1,10 @@
+// Stripe SDK/library to deal with FRONTEND
+
+import { loadStripe } from '@stripe/stripe-js'
+
+export async function getStripeJs() {
+  const stripeJs = await loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
+    )
+  return stripeJs
+}
