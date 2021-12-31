6509589e5b18dea88db06c0f1da22d414e1c7fc7
marcusungarette <marcusungarette@gmail.com>
2021-12-30 23:43:48 -0300

    Feat-Get from PrismicClient {req, params} and render unique post by route

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,60 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head'
+import PrismicDom from 'prismic-dom'
+import { getSession } from 'next-auth/react';
+import { getPrismicClient } from '../../services/prismic';
+
+
+interface PostsProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostsProps) {
+  return (
+    <>
+    <Head>
+      <title>{post.title} | Ignews</title>
+    </Head>
+
+    <main>
+      <article>
+        <h1>{post.title}</h1>
+        <time>{post.updatedAt}</time>
+        <div dangerouslySetInnerHTML={{ __html: post.content }}/>
+      </article>
+    </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const { data: session } = await getSession({ req })
+  
+  const { slug } = params;
+
+  const prismic = getPrismicClient(req)
+
+  const response = await prismic.getByUID('post', String(slug), {})
+  
+  const post = {
+    slug,
+    title: PrismicDom.RichText.asText(response.data.title),
+    content: PrismicDom.RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+  
+  return {
+    props: {
+      post
+      }
+    }
+  }
