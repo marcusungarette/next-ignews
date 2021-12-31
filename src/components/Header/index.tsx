8888c3bd85909e0c031bcfefdf61f972e20e301f
marcusungarette <marcusungarette@gmail.com>
2021-12-30 21:55:39 -0300

    Feat-Transitions between routes /home and /posts using `next/link`

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -7,11 +8,15 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="/">
+          <Link href="/">
+          <a className={styles.active}>
             Home
           </a>
-          <a href="posts">Posts</a>
-        </nav>
+          </Link>
+          <Link href="posts">
+          <a >Posts</a>
+          </Link>
+        </nav>  
         <SignInButton />
       </div>
     </header>
