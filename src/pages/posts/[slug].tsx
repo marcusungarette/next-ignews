4569d2ead3fb672df3d8979a61363b0c34c0b2b5
marcusungarette <marcusungarette@gmail.com>
2022-01-02 16:36:31 -0300

    Chore-Validation if  User have an ActiveSubscription they cannot access the private `posts` - Need to be reviewed because redirect is not working - cannot read a null property

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,9 +37,17 @@ export default function Post({ post }: PostsProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req })
-  console.log(session)
   
   const { slug } = params;
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
